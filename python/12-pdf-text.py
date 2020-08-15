07b7daf2b095dab9456c46bdef06443fe8c53d28
Dylan Oldfield <dylan.oldfield@yahoo.com>
2020-08-15 11:43:15 +1000

    updated python pdf file to read flash card file for CFA

--- python/12-pdf-text.py
+++ python/12-pdf-text.py
@@ -1,5 +1,72 @@
 import boto3
 import time
+import webbrowser, os
+import json
+import webbrowser, os
+import io
+from io import BytesIO
+import sys
+import csv
+from pprint import pprint
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def get_rows_columns_map(table_result, blocks_map):
+    rows = {}
+    for relationship in table_result['Relationships']:
+        if relationship['Type'] == 'CHILD':
+            for child_id in relationship['Ids']:
+                cell = blocks_map[child_id]
+                if cell['BlockType'] == 'CELL':
+                    row_index = cell['RowIndex']
+                    col_index = cell['ColumnIndex']
+                    if row_index not in rows:
+                        # create new row
+                        rows[row_index] = {}
+
+                    # get the text value
+                    rows[row_index][col_index] = get_text(cell, blocks_map)
+    return rows
+
+
+def get_text(result, blocks_map):
+    text = ''
+    if 'Relationships' in result:
+        for relationship in result['Relationships']:
+            if relationship['Type'] == 'CHILD':
+                for child_id in relationship['Ids']:
+                    word = blocks_map[child_id]
+                    if word['BlockType'] == 'WORD':
+                        text += word['Text'] + ' '
+                    if word['BlockType'] == 'SELECTION_ELEMENT':
+                        if word['SelectionStatus'] =='SELECTED':
+                            text +=  'X '
+    return text
+
+
+
+
+def generate_table_csv(table_result, blocks_map, table_index):
+    rows = get_rows_columns_map(table_result, blocks_map)
+
+    table_id = 'Table_' + str(table_index)
+
+    # get cells.
+    csv = 'Table: {0}\n\n'.format(table_id)
+
+    for row_index, cols in rows.items():
+
+        for col_index, text in cols.items():
+            csv += '{}'.format(text) + ","
+        csv += '\n'
+
+    csv += '\n\n\n'
+    return csv
 
 def startJob(s3BucketName, objectName):
     response = None
@@ -37,7 +104,7 @@ def getJobResults(jobId):
 
     client = boto3.client('textract')
     response = client.get_document_text_detection(JobId=jobId)
-    
+
     pages.append(response)
     print("Resultset page recieved: {}".format(len(pages)))
     nextToken = None
@@ -58,18 +125,83 @@ def getJobResults(jobId):
     return pages
 
 # Document
-s3BucketName = "ki-textract-demo-docs"
-documentName = "Amazon-Textract-Pdf.pdf"
+def main():
+    s3BucketName = "amazon-texttract-cfaflashcard"
+    documentName = "Flashcards_List_cropped.pdf"
+    jobId = startJob(s3BucketName, documentName)
+    print("Started job with id: {}".format(jobId))
+    if(isJobComplete(jobId)):
+        response = getJobResults(jobId)
+        #print(response)
+        # Print detected text
+        blocks_map = {}
+        table_blocks = []
+        definitions = []
+        counter = 0
+        key = ''
+        value = ''
+        holder = ''
 
-jobId = startJob(s3BucketName, documentName)
-print("Started job with id: {}".format(jobId))
-if(isJobComplete(jobId)):
-    response = getJobResults(jobId)
+        for resultPage in response:
+            for block in resultPage["Blocks"]:
+                # Get the text blocks
+                blocks_map[block['Id']] = block
+                if block['BlockType'] == "TABLE":
+                    print("table found.")
+                    table_blocks.append(block)
 
-#print(response)
+                # since the definitions come in groups of 3, I create a counter to pull the number and the word and its definition
+                if block['BlockType'] == "LINE":
+                    text = block["Text"]
+                    # Check if the line is numeric signifying a new definition
+                    if(is_number(text)):
+                        if counter > 1:
+                            tup = (key,value,holder)
+                            definitions.append(tup)
+                            print(key + ": "+value)
+                            key=''
+                            value = ''
+                            holder=''
+                        counter = 1
+                        continue
+                    # assings the word, definition and category
+                    # word
+                    if(counter == 1):
+                        key = text
+                        counter +=1
+                        continue
+                    # definition
+                    if(counter >= 2):
+                        value += " " +text
+                        counter +=1
+                        continue
+                    # # category
+                    # if(counter == 3):
+                    #     holder = text
+                    #     counter +=1
+                    #     continue
+                    # # remainder of the definition
+                    # if(counter > 3):
+                    #     value += " " + text
+                    #     continue
 
-# Print detected text
-for resultPage in response:
-    for item in resultPage["Blocks"]:
-        if item["BlockType"] == "LINE":
-            print ('\033[94m' +  item["Text"] + '\033[0m')
+
+        if len(table_blocks) <= 0:
+            print("No Tables Found")
+
+        print("Tables Processed... Moving to CSV")
+        output_csv = ''
+        for index, table in enumerate(table_blocks):
+            output_csv += generate_table_csv(table, blocks_map, index +1)
+            output_csv += '\n\n'
+
+        output_file = 'output.csv'
+        # replace content
+        with open(output_file, "wt") as fout:
+            writer = csv.writer(fout, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for tup in definitions:
+                writer.writerow(tup)
+        # show the results
+        print('Done. CSV OUTPUT FILE: ', output_file)
+if __name__ == "__main__":
+    main()
